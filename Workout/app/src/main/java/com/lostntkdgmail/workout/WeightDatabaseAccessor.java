67ffde3ddb8bf874e537fb673646c43c9a59498c
Tyler <tha7556@uncw.edu>
2017-03-08 20:08:20 -0500

    Now saves the number of the month instead of the name

--- Workout/app/src/main/java/com/lostntkdgmail/workout/WeightDatabaseAccessor.java
+++ Workout/app/src/main/java/com/lostntkdgmail/workout/WeightDatabaseAccessor.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class WeightDatabaseAccessor extends DatabaseAccessor {
@@ -21,12 +22,15 @@ public class WeightDatabaseAccessor extends DatabaseAccessor {
     }
     public boolean insert(String user,String type,String lift, int weight, int reps) {
         SQLiteDatabase db = this.getWritableDatabase();
-        String date = new Date().toString();
-        date = date.substring(4,10)+ date.substring(23);
-        Log.d("Debug","Inserting: \"" + user +", "+ date +", "+ type +", "+ lift +", "+ weight +", "+ reps + "\" into \"" + tableName + "\"");
+        Date date = new Date();
+        String printDate = new Date().toString();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        printDate = printDate.substring(24)+"/"+(calendar.get(Calendar.MONTH)+1)+"/"+printDate.substring(8,10) ;
+        Log.d("Debug","Inserting: \"" + user +", "+ printDate +", "+ type +", "+ lift +", "+ weight +", "+ reps + "\" into \"" + tableName + "\"");
         ContentValues contentValues = new ContentValues();
         contentValues.put(col[1],user);
-        contentValues.put(col[2],date);
+        contentValues.put(col[2],printDate);
         contentValues.put(col[3],type);
         contentValues.put(col[4],lift);
         contentValues.put(col[5],weight);
@@ -49,42 +53,42 @@ public class WeightDatabaseAccessor extends DatabaseAccessor {
      * @param sorting SQLite sorting algorithm
      * @return Cursor object with all selected values
      */
-    public Cursor getCursor(String user, String type, String lift, String sorting) {
+    public Cursor getCursor(String user, String type, String lift, String sorting, String limit) {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor res = null;
         if(user != null) {
             if (type != null && lift != null) {
                 String[] selection = {type, lift, user};
-                res = db.query(tableName, col, col[3] + " = ? and " + col[4] + " = ? and " + col[1] + " = ?", selection, null, null,sorting);
+                res = db.query(tableName, col, col[3] + " = ? and " + col[4] + " = ? and " + col[1] + " = ?", selection, null, null,sorting,limit);
             }
             else if (type != null) {
                 String[] selection = {type, user};
-                res = db.query(tableName, col, col[3] + " = ? and " + col[1] + " = ?", selection, null, null,sorting);
+                res = db.query(tableName, col, col[3] + " = ? and " + col[1] + " = ?", selection, null, null,sorting,limit);
             }
             else if (lift != null) {
                 String[] selection = {lift, user};
-                res = db.query(tableName, col, col[4] + " = ? and " + col[1] + " = ?", selection, null, null,sorting);
+                res = db.query(tableName, col, col[4] + " = ? and " + col[1] + " = ?", selection, null, null,sorting,limit);
             }
             else {
                 String[] selection = { user};
-                res = db.query(tableName, col, col[1] + " = ?", selection, null, null,sorting);
+                res = db.query(tableName, col, col[1] + " = ?", selection, null, null,sorting,limit);
             }
         }
         else {
             if(type != null && lift != null) {
                 String[] selection = {lift, type};
-                res = db.query(tableName, col, col[4] + " = ? and " + col[3] + " = ?", selection, null, null, sorting);
+                res = db.query(tableName, col, col[4] + " = ? and " + col[3] + " = ?", selection, null, null, sorting,limit);
             }
             else if(type != null) {
                 String[] selection = {type};
-                res = db.query(tableName, col, col[3] + " = ?", selection, null, null,sorting);
+                res = db.query(tableName, col, col[3] + " = ?", selection, null, null,sorting,limit);
             }
             else if(lift != null) {
                 String[] selection = {lift};
-                res = db.query(tableName, col, col[4] + " = ?", selection, null, null,sorting);
+                res = db.query(tableName, col, col[4] + " = ?", selection, null, null,sorting,limit);
             }
             else {
-                res = db.query(tableName, col, null, null, null, null, sorting);
+                res = db.query(tableName, col, null, null, null, null, sorting,limit);
             }
         }
         return res;
@@ -97,7 +101,7 @@ public class WeightDatabaseAccessor extends DatabaseAccessor {
      * @return Cursor object with all selected values
      */
     public Cursor getCursor(String user, String type, String lift) {
-        return getCursor(user,type,lift, col[2] + " ASC, " + col[3] + " ASC, " + col[5] + " ASC, " + col[2] + " ASC");
+        return getCursor(user,type,lift, col[1] + " ASC, " + col[2] + " ASC, " + col[3] + " ASC, " + col[4] + " ASC, " + col[0] + " ASC",null);
     }
     public boolean updateData(String id,String user,String date, String type,String lift, int weight, int reps) {
         Log.d("Debug","Replacing id: " + id + " with: " + user +" "+ date +" "+ type +" "+ lift +" "+ weight +" "+ reps + " into " + tableName);
