5d02adafca65a14f73ab6330b744111d3683f17d
Tyler <tha7556@uncw.edu>
2017-03-08 20:08:58 -0500

    Recieves intents and shows 3 previous entries

--- Workout/app/src/main/java/com/lostntkdgmail/workout/WeightSelection.java
+++ Workout/app/src/main/java/com/lostntkdgmail/workout/WeightSelection.java
@@ -1,6 +1,7 @@
 package com.lostntkdgmail.workout;
 
 
+        import android.database.Cursor;
         import android.os.Bundle;
         import android.app.Activity;
         import android.util.Log;
@@ -11,8 +12,12 @@ package com.lostntkdgmail.workout;
         import android.widget.NumberPicker;
         import android.widget.Toast;
 
+        import java.lang.reflect.Array;
+        import java.util.ArrayList;
+
 
 public class WeightSelection extends Activity {
+    private String type, lift,user;
     private int digit1 = 0;
     private int digit2 = 0;
     private int digit3 = 0;
@@ -24,8 +29,11 @@ public class WeightSelection extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("Debug","Launching Activity");
+        Log.d("Debug","Launching Activity: Weight Selection");
         setContentView(R.layout.weight_selection);
+        type = getIntent().getStringExtra("TYPE");
+        lift = getIntent().getStringExtra("LIFT");
+        user = "Tyler";
 
         setUpSeekBar();
         setUpNumberPickers();
@@ -33,24 +41,28 @@ public class WeightSelection extends Activity {
         Button submit = (Button)findViewById(R.id.button);
         wdb = new WeightDatabaseAccessor(this);
         ldb = new LiftDatabaseAccessor(this);
-        ldb.insert("Arms","Curls");
-        //ldb.fillWithData();
 
+        for(int[] s : getPreviousWeights()) {
+            Log.d("Debug",s[0]+" "+s[1]);
+        }
 
     }
     @Override
     protected void onDestroy() {
-        Log.d("Debug","onDestroy() called");
+        Log.d("Debug","onDestroy() called for WeightSelection");
         wdb.close();
+        ldb.close();
         super.onDestroy();
     }
     public void submitWeight(View view) {
         int weight = digit1*100+digit2*10+digit3;
         if(reps > 0 && weight > 0) {
 
-            boolean insertResult = wdb.insert("Tyler", "Arms", "Curls", weight, reps);
-            if(insertResult)
-                Toast.makeText(getApplicationContext(), "Submitted "+weight+" lbs!", Toast.LENGTH_SHORT).show();
+            boolean insertResult = wdb.insert("Tyler", type, lift, weight, reps);
+            if(insertResult) {
+                Toast.makeText(getApplicationContext(), "Submitted!", Toast.LENGTH_SHORT).show();
+                getPreviousWeights();
+            }
             else
                 Toast.makeText(getApplicationContext(),"Failed to submit",Toast.LENGTH_SHORT).show();
         }
@@ -124,6 +136,61 @@ public class WeightSelection extends Activity {
             }
         });
         sBarText.setText(sBar.getProgress() + " reps");
+    }
+    public ArrayList<int[]> getPreviousWeights() {
+        Cursor c = wdb.getCursor(user,type,lift,wdb.getCol()[0]+" DESC","3");
+        ArrayList<int[]> result = new ArrayList<int[]>(3);
+        while(c.moveToNext()) {
+            int[] arr = {Integer.parseInt(c.getString(5)),Integer.parseInt(c.getString(6))};
+            result.add(arr);
+        }
+        TextView weight1 = (TextView)findViewById(R.id.Weight1);
+        TextView weight2 = (TextView)findViewById(R.id.Weight2);
+        TextView weight3 = (TextView)findViewById(R.id.Weight3);
+        TextView rep1 = (TextView)findViewById(R.id.Rep1);
+        TextView rep2 = (TextView)findViewById(R.id.Rep2);
+        TextView rep3 = (TextView)findViewById(R.id.Rep3);
+        if(result.size() == 3) {
+            weight3.setText(result.get(0)[0]+" lbs");
+            rep3.setText(result.get(0)[1]+ " reps");
+
+            weight2.setText(result.get(1)[0]+" lbs");
+            rep2.setText(result.get(1)[1]+ " reps");
+
+            weight1.setText(result.get(2)[0]+" lbs");
+            rep1.setText(result.get(2)[1]+ " reps");
+        }
+        else if(result.size() == 2) {
+            weight2.setText(result.get(1)[0]+" lbs");
+            rep2.setText(result.get(1)[1]+ " reps");
+
+            weight3.setText(result.get(0)[0]+" lbs");
+            rep3.setText(result.get(0)[1]+ " reps");
+
+            weight1.setText("-- lbs");
+            rep1.setText("-- reps");
+        }
+        else if(result.size() == 1) {
+            weight3.setText(result.get(0)[0]+" lbs");
+            rep3.setText(result.get(0)[1]+ " reps");
+
+            weight2.setText("-- lbs");
+            rep2.setText("-- reps");
+
+            weight1.setText("-- lbs");
+            rep1.setText("-- reps");
+        }
+        else if(result.size() ==0) {
+            weight1.setText("-- lbs");
+            rep1.setText("-- reps");
+
+            weight2.setText("-- lbs");
+            rep2.setText("-- reps");
+
+            weight3.setText("-- lbs");
+            rep3.setText("-- reps");
+        }
+        return result;
     }
 
 
