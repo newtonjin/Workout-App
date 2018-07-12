709a28a3007c4b2e831c8cfd50bd035d393f2458
Tyler Atkinson <tyler.atkinson@gmail.com>
2018-07-12 09:08:17 -0400

    Added some log statements

--- Workout/app/src/main/java/com/lostntkdgmail/workout/main/BaseFragment.java
+++ Workout/app/src/main/java/com/lostntkdgmail/workout/main/BaseFragment.java
@@ -1,6 +1,8 @@
 package com.lostntkdgmail.workout.main;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
 public abstract class BaseFragment extends Fragment {
     public void reload() {
@@ -9,9 +11,24 @@ public abstract class BaseFragment extends Fragment {
         super.onResume();
         if(getContext() != null)
             reload();
+        Log.d(getClass().getSimpleName(),"onResume called");
     }
     public void setUserVisibleHint(boolean isVisible) {
         if(isVisible && getContext() != null)
             reload();
+        Log.d(getClass().getSimpleName(),"setUserVisibleHint("+isVisible+") called");
     }
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(getClass().getSimpleName(),"onAttach called");
+    }
+    public void onDetach() {
+        super.onDetach();
+        Log.d(getClass().getSimpleName(),"onDetach called");
+    }
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(getClass().getSimpleName(), "onDestroy called");
+    }
+
 }
